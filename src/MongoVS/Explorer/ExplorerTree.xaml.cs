7e90f8ce73b0c62967f9195e1b65bfc740648051
Craig Wilson <craiggwilson@gmail.com>
2013-09-30 17:09:22 -0500

    tree view images now work in both light and dark themes.

--- src/MongoVS/Explorer/ExplorerTree.xaml.cs
+++ src/MongoVS/Explorer/ExplorerTree.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.VisualStudio.PlatformUI;
 using MongoDB.VisualStudio.Explorer.ViewModels;
 
 namespace MongoDB.VisualStudio.Explorer
